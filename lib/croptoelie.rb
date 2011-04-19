de8bf44878eea5e6a381b32d630c3a8c04c5641a
Bèr Kessels <ber@webschuur.com>
2011-04-19 17:04:00 +0200

    Remove by_search. Its results were not much better, but the performance is unacceptable and hard to imrpove.

--- lib/croptoelie.rb
+++ lib/croptoelie.rb
@@ -27,23 +27,13 @@ class CropToelie
     return CropToelie.new(image)
   end
 
-  # Crops an image to width x height
-  # 
-  # If you want speed, choose :by_trim. It will use smart_crop_by_trim().
-  # If you have a very crowded image, an image with lots of color, or dark and light
-  #  spots, the much slower :by_search will give better results. It will use
-  #  smart_crop_by_search().  
-  def smart_crop(width, height, method = :by_trim)
-    sq = square(width, height, method)
+  # Crops an image to width x height 
+  def smart_crop(width, height)
+    sq = square(width, height)
     return @image.crop!(sq[:left], sq[:top], width, height, true)
   end
 
   # Squares an image (with smart_square) and then scales that to width, heigh
-  # 
-  # If you want speed, choose :by_trim. It will use smart_crop_by_trim().
-  # If you have a very crowded image, an image with lots of color, or dark and light
-  #  spots, the much slower :by_search will give better results. It will use
-  #  smart_crop_by_search().  
   def smart_crop_and_scale(width, height)
     smart_square    
     return @image.scale!(width, height)
@@ -59,7 +49,7 @@ class CropToelie
         crop_height = crop_width = @columns
       end
 
-      sq = square(crop_width, crop_height, :by_trim)
+      sq = square(crop_width, crop_height)
       @image.crop!(sq[:left], sq[:top], crop_width, crop_height, true)
     end
     
@@ -68,14 +58,9 @@ class CropToelie
   
   # Finds the most interesting square with size width x height.
   # 
-  # See smart_crop documentation for explanation about the method
   # Returns a hash {:left => left, :top => top, :right => right, :bottom => bottom}
-  def square(width, height, method = :by_trim)
-    if method == :by_trim
-      return smart_crop_by_trim(width, height)
-    else
-      return smart_crop_by_search(width, height)
-    end
+  def square(width, height)
+    return smart_crop_by_trim(width, height)
   end
   
   private
@@ -86,41 +71,6 @@ class CropToelie
       return (@columns > @width) && (@rows < @height)
     end
 
-    # Find Entropy by moving the "to be cropped" area over the image and 
-    # recording the entropy of each such square. 
-    # The square with the highest entropy is considered the most interesting and
-    # cropped out of the original.
-    # NOTE: this method is very slow compared to smart_crop_by_trim. 
-    def smart_crop_by_search(requested_x, requested_y)
-      left, top     = 0, 0
-      right, bottom = requested_x, requested_y
-
-      # Create a hash with all entropies
-      entropies = {}
-              
-      # start in left-top corner, walk to right, with steps of 10 px.
-      while (bottom <= @rows)
-        while (right <= @columns)
-          square = {:left => left, :top => top, :right => right, :bottom => bottom}
-          entropies[square] = entropy_slice(@image, left, top, right - left, bottom - top)
-          
-          left += @step_size
-          right += @step_size
-        end
-        # @TODO last item is the one that goes over the edge, or touches the edge.
-        left = 0
-        right = requested_x
-        top += @step_size
-        bottom += @step_size
-      end
-
-      # Find the square with highest entropy
-      best = entropies.max_by{|s| s[1]}[0]
-      
-      # chop that one out
-      best
-    end
-  
     def smart_crop_by_trim(requested_x, requested_y)
       left, top = 0, 0
       right, bottom = @columns, @rows
